c0f0f9d09e5bf1416ea1bb4db68d8b747c1c52fb
bvgaurav4 <bv.gaurav4@gmail.com>
2024-12-09 13:00:14 +0530

    arcade machine

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -1,12 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Base from './Base.tsx'
 import Lays from './lays.tsx'
-import { Sphere } from 'three'
-import Lays2 from './test.tsx'
 
-import { Stack, Button } from '@mantine/core';
 createRoot(document.getElementById('root')!).render(
   <StrictMode >
     <div>
