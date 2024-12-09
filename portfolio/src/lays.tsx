c43015f51d01887f388da99580123bbd35271233
bvgaurav4 <bv.gaurav4@gmail.com>
2024-12-09 12:57:11 +0530

    arcade machine

--- portfolio/src/lays.tsx
+++ portfolio/src/lays.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import Sector from './secotr'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { depth, sin } from 'three/webgpu'
-
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 export default function Lays() {
   const mountRef = useRef<HTMLDivElement>(null)
   const audioRef = useRef<HTMLAudioElement>(null)
@@ -47,7 +47,7 @@ export default function Lays() {
       scene.add(glassSlab);
 
       const daft_texture= new THREE.TextureLoader().load("src/assets/Daft_Punk_-_Random_Access_Memories.jpg")
-      const daft = new THREE.CylinderGeometry(100, 100, 5,64)
+      const daft = new THREE.CylinderGeometry(110, 110, 5,64)
       const daftMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff 
         ,map:daft_texture
       })
@@ -78,38 +78,69 @@ export default function Lays() {
     {
       let add= 0
       radius+=25
-      console.log(radius)
-      for( var i=0;i<19;i++)
+      for( var i=0;i<16;i++)
         {
-          add+=Math.PI / 9;
-          const shape2 = new THREE.Shape();
-          shape2.moveTo(0, 0); 
-          shape2.arc(0, 0, radius, startAngle, endAngle, false);
-          shape2.lineTo(0, 0); 
-          
-          const innerShape = new THREE.Path();
-          innerShape.moveTo(0, 0);
-          innerShape.arc(0, 0, radius-10, startAngle, endAngle, false);
-          innerShape.lineTo(0, 0);
-          
-          shape2.holes.push(innerShape);
-          
-          const geometryRect = new THREE.ExtrudeGeometry(shape2, extrudeSettings);
-          const materialRect = new THREE.MeshBasicMaterial({ color: 0xff0000 }); 
-          const rectMesh = new THREE.Mesh(geometryRect, materialRect);
-          rectMesh.rotation.x += Math.PI / 2
-          scene.add(rectMesh);
-          rectMesh.rotation.z+=add
-          rectMesh.position.set(r*Math.cos(add), -15,r*Math.sin(add));
-          
+          add+=Math.PI/8;
+          const geometry = new THREE.CylinderGeometry( radius-15, radius, 0, 64,1,true,add,endAngle); 
+          const material = new THREE.MeshBasicMaterial( {color: 0xffff00} ); 
+          const cylinder = new THREE.Mesh( geometry, material );
+          cylinder.position.set(0, -15,0 );
+          scene.add( cylinder );
         }
     }
 
+
+
+
+
+    const loader = new GLTFLoader();
+
+    loader.load( 'src/assets/cat_arcade_machine4k.glb', function ( gltf ) {
+      const model = gltf.scene
+      model.scale.set(20, 20, 20) // Scale the model to twice its original size
+      scene.add( model );
+
     
+    }, undefined, function ( error ) {
+    
+      console.error( error );
+    
+    } );
+
+
+
+    loader.load( 'src/assets/postapocaliptic_diablo_arcade_machine (1).glb', function ( gltf ) {
+      const model = gltf.scene
+      model.scale.set(20, 20, 20) // Scale the model to twice its original size
+      model.position.set(20,20,20)
+      scene.add( model );
+
+    
+    }, undefined, function ( error ) {
+    
+      console.error( error );
+    
+    } );
+
+
+    loader.load( 'src/assets/blade_runner_arcade_cabinet.glb', function ( gltf ) {
+      const model = gltf.scene
+      model.scale.set(0.090, 0.090, 0.090) // Scale the model to twice its original size
+      scene.add( model );
+
+    
+    }, undefined, function ( error ) {
+    
+      console.error( error );
+    
+    } );
+
+
+
 
     const animate = () => {
       requestAnimationFrame(animate)
-      daftMesh.rotation.y += 0.015
+      daftMesh.rotation.y += 0.013
       orbit.update()
       renderer.render(scene, camera)
     }
@@ -129,8 +160,9 @@ export default function Lays() {
     }
   }, [])
   return <div ref={mountRef}>
-        <audio    ref={audioRef} loop>
+        {/* <audio    ref={audioRef} loop>
         <source src="src/assets/Doin it Right (Official Audio).mp3" type="audio/mpeg" />
-    </audio>
+    </audio> */}
+
   </div>
 }
