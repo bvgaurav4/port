d7cfebb1151fe9ca77b191189acfea121e06d9eb
bvgaurav4 <bv.gaurav4@gmail.com>
2025-03-09 14:18:50 +0530

    buttons done

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Lays from './lays.tsx'
-import Test from './test.tsx'
+// import Test from './test.tsx'
 
 import './index.css'
 createRoot(document.getElementById('root')!).render(
