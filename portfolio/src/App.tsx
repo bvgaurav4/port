9429b4a8fbfb38f630b9126cea7cfb0fded89663
bvgaurav4 <bv.gaurav4@gmail.com>
2025-03-15 13:16:10 +0530

    almost done

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,12 +1,12 @@
-import { Routes, Route } from "react-router-dom";
-import Landing from "./landing";
-import Lays from "./lays";
-import './index.css'
-export default function App() {
-  return (
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/bv.gaurav" element={<Lays />} />
-      </Routes>
-  );
-}
+// import { Routes, Route } from "react-router-dom";
+// import Landing from "./landing";
+// import Lays from "./lays";
+// import './index.css'
+// export default function App() {
+//   return (
+//       <Routes>
+//         <Route path="/" element={<Landing />} />
+//         <Route path="/bv.gaurav" element={<Lays />} />
+//       </Routes>
+//   );
+// }
--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './index.css'
 import Lays from "./lays";
-import Landing from "./landing";
+import Landing from "./landing.tsx";
 // import App from "./test";
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
